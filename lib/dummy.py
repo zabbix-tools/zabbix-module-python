3562e0f088d1944c86820e1672e0a90792f26985
Ryan Armstrong <ryan@cavaliercoder.com>
2016-09-29 19:59:13 +0800

    Added docs to dummy module

--- lib/dummy.py
+++ lib/dummy.py
@@ -1,19 +1,27 @@
+"""dummy module for the Zabbix agent"""
+
 import zabbix_module
 import random
 
 def ping(request):
+  """agent item python.ping"""
+
   if len(request.params) != 0:
     raise ValueError('Invalid number of parameters')
 
   return 1
 
 def echo(request):
+  """agent item python.echo[...]"""
+
   if len(request.params) == 0:
     raise ValueError('Invalid number of parameters')
 
   return " ".join(request.params)
 
 def rand(request):
+  """agent item python.random[lower,upper]"""
+
   if len(request.params) != 2:
     raise ValueError('Invalid number of parameters')
 
@@ -26,9 +34,14 @@ def rand(request):
   return random.randrange(l, u)
 
 def zbx_module_init():
+  """initialize this module"""
+
+  zabbix_module.info("Initalized dummy module")
   random.seed()
 
 def zbx_module_item_list():
+  """register available module items"""
+
   return [
     zabbix_module.AgentItem("python.ping",   fn = ping),
     zabbix_module.AgentItem("python.echo",   fn = echo, test_param = [ 'hello', 'world' ]),
